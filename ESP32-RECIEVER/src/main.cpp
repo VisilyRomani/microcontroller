f42f90c8e6078a7b2b61a5b85e441ca157c69e23
Visily <36494994+VisilyRomani@users.noreply.github.com>
2024-08-28 14:00:50 -0600

    update reader and rfid

--- ESP32-RECIEVER/src/main.cpp
+++ ESP32-RECIEVER/src/main.cpp
@@ -1,54 +1,65 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-// THIS is on board 0xa0, 0xdd, 0x6c, 0x85, 0xb5, 0xac
+
+// A0:DD:6C:04:38:38
 
 // Structure example to receive data
 // Must match the sender structure
-typedef struct struct_message {
-    char a[32];
-    int b;
-    float c;
-    bool d;
+typedef struct struct_message
+{
+  bool isOpen;
 } struct_message;
 
 // Create a struct_message called myData
 struct_message myData;
 
 // callback function that will be executed when data is received
-void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
   memcpy(&myData, incomingData, sizeof(myData));
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  Serial.print("Char: ");
-  Serial.println(myData.a);
-  Serial.print("Int: ");
-  Serial.println(myData.b);
-  Serial.print("Float: ");
-  Serial.println(myData.c);
-  Serial.print("Bool: ");
-  Serial.println(myData.d);
-  Serial.println();
+  Serial.println(myData.isOpen);
+
+  if (myData.isOpen == true)
+  {
+    digitalWrite(16, LOW);
+  }
+  else
+  {
+    digitalWrite(16, HIGH);
+  }
 }
- 
-void setup() {
+
+void setup()
+{
   // Initialize Serial Monitor
   Serial.begin(115200);
-  
+  pinMode(16, OUTPUT);
+
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
   // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
+  if (esp_now_init() != ESP_OK)
+  {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  
+  Serial.println(WiFi.macAddress());
+
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_register_recv_cb(OnDataRecv);
+  digitalWrite(16, HIGH);
+
 }
- 
-void loop() {
- Serial.println(myData.a);
+
+void loop()
+{
+  // digitalWrite(16, HIGH);
+  // Serial.println("HIGH");
+  // delay(5000);
+  // digitalWrite(16, LOW);
+  // Serial.println("LOW");
+  // delay(5000);
 }
--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,20 +1,30 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <esp_now.h>
+#include <MFRC522.h>
+#include <SPI.h>
 
 // THIS is on board 0xa0, 0xdd, 0x6c, 0x04, 0x38, 0x38
 
+#define RST_PIN 16
+#define MOSI 18
+#define MISO 17
+#define SCK 19
+#define SS 21
+byte pins[] = {RST_PIN, MOSI, MISO, SCK, SS};
 
+MFRC522 mfrc522;
 typedef struct struct_message
 {
-  char a[32];
-  int b;
-  float c;
-  bool d;
+  bool isOpen;
 } struct_message;
 
+String card_uid;
+
+String card_serial;
 struct_message myData;
 esp_now_peer_info_t peerInfo;
+uint8_t broadcastAddress[] = {0xa0, 0xdd, 0x6c, 0x04, 0x38, 0x38};
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 {
@@ -22,46 +32,115 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-uint8_t broadcastAddress[] = {0xa0, 0xdd, 0x6c, 0x85, 0xb5, 0xac};
+void dump_byte_array(byte *buffer, byte bufferSize)
+{
+  for (byte i = 0; i < bufferSize; i++)
+  {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+String byteArrayToHexString(byte *buffer, byte bufferSize)
+{
+  String result = "";
+  for (byte i = 0; i < bufferSize; i++)
+  {
+    if (buffer[i] < 0x10)
+    {
+      result += " 0";
+    }
+    else
+    {
+      result += " ";
+    }
+    result += String(buffer[i], HEX);
+  }
+  return result;
+}
+
+char **read_string_array(byte *buffer, byte bufferSize)
+{
+  // Allocate memory for the array of strings
+  char **result = new char *[bufferSize];
+  for (byte i = 0; i < bufferSize; i++)
+  {
+    // Allocate memory for each string (3 characters: " 0x" + null terminator)
+    result[i] = new char[4];
+    // Format the byte as a string and store it in the array
+    sprintf(result[i], "%02X", buffer[i]);
+  }
+  return result;
+}
+
+void verify_card(byte card_id, bool isOpen)
+{
+  // if (card_id)
+  // {
+  myData.isOpen = isOpen;
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+
+  if (result == ESP_OK)
+  {
+    Serial.println("Sent with success");
+  }
+  else
+  {
+    Serial.println("Error sending the data");
+  }
+  // }
+}
+
 
 void setup()
 {
   Serial.begin(115200);
+
+  // Setup rfid
+  SPI.begin(SCK, MISO, MOSI, SS); // SCK, MISO, MOSI, SS
+  mfrc522.PCD_Init(SS, RST_PIN);
+
+  // Setup ESP Now
   WiFi.mode(WIFI_STA);
   Serial.println(WiFi.macAddress());
-
   if (esp_now_init() != ESP_OK)
   {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
   esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
 
-    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
     Serial.println("Failed to add peer");
     return;
   }
 }
-
 void loop()
 {
-  // Set values to send
-  strcpy(myData.a, "THIS IS A CHAR");
-  myData.b = random(1,20);
-  myData.c = 1.2;
-  myData.d = false;
+
+  Serial.println(mfrc522.PICC_ReadCardSerial());
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
+  {
+      Serial.println("Card Added");
+
+      card_uid = byteArrayToHexString(mfrc522.uid.uidByte, mfrc522.uid.size);
+      Serial.println(card_uid);
+
+      // Halt PICC
+      mfrc522.PICC_HaltA();
+      
+      // Stop encryption on PCD
+      mfrc522.PCD_StopCrypto1();
+      }
+    
   
-  // Send message via ESP-NOW
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-   
-  if (result == ESP_OK) {
-    Serial.println("Sent with success");
-  }
-  else {
-    Serial.println("Error sending the data");
-  }
-  delay(2000);
+  delay(100);
 }
